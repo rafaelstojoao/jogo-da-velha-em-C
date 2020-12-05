f1ef3ae36c5ce8ffea93ae8f539f0427f593c89d
Rafael Stoffalette João <rafaelcompp@gmail.com>
2020-12-05 09:36:42 -0300

    inclusão do código do jogo da velha
    Jogo implementado em linguagem C, pela turma de Linguages de programação do curso de Tecnologia em Mecatrônica Industrial no Instituto Federal de Educação, Ciência e Tecnologia de São Paulo (IFSP), campus Birigui.

---
+++ velha2020.c
@@ -0,0 +1,225 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int geraValorAleatorio(int divisor){ //Cada vez que essa função for chamada ela retona um valor aleatório entre [0 e 2[
+	int valorAleatorio;
+	valorAleatorio = rand()%divisor; 
+	return valorAleatorio;
+}
+
+
+int verificaResultado(char tabuleiro[3][3],int quantJogadas){
+	int resultado = 0;
+	/*	
+	-------
+	|(0,0)|(0,1)|(0,2)|
+	|(1,0)|(1,1)|(1,2)|
+	|(2,0)|(2,1)|(2,2)|
+	-------
+	*/
+
+	//primeira linha
+	if(tabuleiro[0][0] != ' ' && tabuleiro[0][0] == tabuleiro[0][1] && tabuleiro[0][1] == tabuleiro[0][2]){
+		printf("\n %c Ganhou na primeira linha",tabuleiro[0][0]);
+		resultado = 1;
+	}
+	//segunda linha
+	else if( tabuleiro[1][0] != ' ' && tabuleiro[1][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[1][2]){
+		printf("\n %c Ganhou na segunda linha",tabuleiro[1][0]);
+		resultado = 1;
+	}
+	// terceira linha
+	else if( tabuleiro[2][0] != ' ' && tabuleiro[2][0] == tabuleiro[2][1] && tabuleiro[2][1] == tabuleiro[2][2]){
+		printf("\n %c Ganhou na terceira linha",tabuleiro[2][0]);
+		resultado = 1;
+	}
+
+	/*	
+	-------
+	|(0,0)|(0,1)|(0,2)|
+	|(1,0)|(1,1)|(1,2)|
+	|(2,0)|(2,1)|(2,2)|
+	-------
+	*/
+	//verificar na primeira coluna
+	else if( tabuleiro[0][0] != ' ' && tabuleiro[0][0] == tabuleiro[1][0] && tabuleiro[1][0] == tabuleiro[2][0]){
+		printf("\n %c Ganhou na primeira coluna", tabuleiro[0][0]);
+		resultado = 1;
+	}
+	//verificando a segunda coluna
+	else if ( tabuleiro[0][1] != ' ' &&  tabuleiro[0][1] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][1]){
+		printf("\n %c Ganhou na segunda coluna",tabuleiro[1][1]);
+		resultado = 1;
+	}
+	else if( tabuleiro[0][2] != ' ' && tabuleiro[0][2] == tabuleiro[1][2] && tabuleiro[1][2] == tabuleiro[2][2]){
+		printf("\n %c Ganhou na terceira coluna!", tabuleiro[1][2]);
+		resultado = 1;
+	}
+
+
+	/* Vamos verificar as diagonais.. COMO??	
+	-------
+	|(0,0)|(0,1)|(0,2)|
+	|(1,0)|(1,1)|(1,2)|
+	|(2,0)|(2,1)|(2,2)|
+	-------
+	*/
+	else if( tabuleiro[0][0] != ' ' && tabuleiro[0][0] == tabuleiro[2][2] && tabuleiro[1][1] == tabuleiro[0][0]){
+		printf("\n %c Ganhou na diagonal principal",tabuleiro[1][1]);
+		resultado = 1;
+	}
+	else if( tabuleiro[0][2] != ' ' && tabuleiro[0][2]==  tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]){
+		printf("\n %c Ganhou na diagonal inversa",tabuleiro[1][1]);
+		resultado = 1;
+	}
+	else if(quantJogadas == 9){
+		printf("\n DEU VELHA!!!!");
+		resultado = 1;
+	}
+	return resultado;
+}
+
+
+void imprimeTabuleiro(char tabuleiro[3][3]){
+	int linha,coluna;
+	for (linha=0;linha<3;linha++){
+		printf("\n");
+		for (coluna=0;coluna<3;coluna++){
+			printf("| %c |",tabuleiro[linha][coluna]);
+		}
+	}
+
+	return;
+}
+
+
+
+int main(void){
+srand(time(0));  			//Essa função  inicializa o contador de tempo para gerar numeros aleatórios	
+
+/*	
+-------
+|X|O|O|
+|O|X|X|
+|O|O|X|
+-------
+*/
+
+char tabuleiro[3][3];
+int linha;
+int coluna;
+	// -------
+	// |(0,0)|(0,1)|(0,2)|
+	// |(1,0)|(1,1)|(1,2)|
+	// |(2,0)|(2,1)|(2,2)|
+	// -------
+
+for (linha =0; linha<3;linha++){
+	for(coluna=0;coluna<3;coluna++){
+		tabuleiro[linha][coluna] = ' ';
+	}
+	
+}
+
+int contadorDeMovimentos = 1;
+int alguemGanhou = 0;//1 se alguem ganhou.
+int linhaJogada,colunaJogada;
+char simboloHumano,simboloComputador,jogadaPassada;
+
+jogadaPassada = 'a';
+int quemJoga = 0; //0 ou 1 onde 0 é o jogador humano, 1: computador
+int quemInicia = geraValorAleatorio(2);
+quemJoga = quemInicia;
+
+
+
+printf("\n Informe se você deseja jogar com 'X' ou 'O'");
+fflush(stdin); //Para prevenir buffer...
+scanf(" %c",&simboloHumano);
+
+if( simboloHumano == 'X')
+	simboloComputador = 'O';
+else
+	simboloComputador = 'X';
+
+
+while(contadorDeMovimentos <= 9 && alguemGanhou == 0){
+	
+	printf("\n Quem joga agora é o:");
+	if(quemJoga == 0)
+		printf(" Humano");
+	else
+		printf(" Computador");
+
+
+	do{
+	    do{
+	    	if(quemJoga == 1)
+	    		linhaJogada = geraValorAleatorio(3);
+	    	else{
+				printf("\n Por favor, informe uma linha onde quer jogar");
+				scanf("%d",&linhaJogada);
+				}	
+		if(linhaJogada < 0 || linhaJogada >= 3)
+				printf("\nMovimento inválido!!!\n");
+		
+
+		}while(linhaJogada<0 ||linhaJogada>=3);
+
+		do{
+			if(quemJoga == 1)
+				colunaJogada = geraValorAleatorio(3);
+			
+			else{
+				printf("\n Por favor, informe uma coluna onde quer jogar");
+				scanf("%d",&colunaJogada);
+			}
+
+			if(colunaJogada<0 || colunaJogada>=3)
+				printf("\n Movimento inválido!!!\n");
+		
+		}while(colunaJogada<0 ||colunaJogada>=3);
+
+		if(tabuleiro[linhaJogada][colunaJogada] != ' '){
+			printf("posição já foi jogada!");
+		}
+
+	}while(tabuleiro[linhaJogada][colunaJogada] != ' ');
+	
+
+	if(quemJoga == 0){
+		quemJoga = 1;
+		tabuleiro[linhaJogada][colunaJogada] = simboloHumano;
+		jogadaPassada = simboloHumano;
+	}else{
+		quemJoga = 0;
+		tabuleiro[linhaJogada][colunaJogada] = simboloComputador;
+		jogadaPassada = simboloComputador;
+	}
+	system("clear"); //( Utilize essa linha se você usa linux)
+	// system("cls"); //(Utilize essa linha se você usa  windows)
+
+
+	
+	imprimeTabuleiro(tabuleiro);
+	alguemGanhou = verificaResultado(tabuleiro,contadorDeMovimentos);
+	contadorDeMovimentos++;
+	
+	if (alguemGanhou == 1){
+		if(quemJoga == 0){
+			printf("\n O vitorioso foi o Computador!!!");
+		}
+		else{
+			printf("\n Vitória do player HUMANO!!!");
+		}
+	}
+
+
+
+
+}
+
+	return 0;
+}
